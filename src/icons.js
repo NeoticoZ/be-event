21e5ba4503aa4ac1eefba722f48862108a0aa23e
NeoticoZ <heriksaraujodias@gmail.com>
2022-04-27 13:04:43 -0300

    feat: added font-awesome icons

---
+++ src/icons.js
@@ -0,0 +1,4 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faRightLong } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faRightLong);
--- src/main.js
+++ src/main.js
@@ -2,4 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "@/styles/global.scss";
 
-createApp(App).mount("#app");
+import { Icon } from "@altipla/fa6-icon";
+import "./icons";
+
+let app = createApp(App);
+app.component(Icon.name, Icon);
+app.mount("#app");
